5c86bf5f5651151692368d4992bd8254cd2257cd
Manobala02 <myself.bala13>
2018-10-25 21:00:30 +0530

    a bsae class

--- src/com/test/GitExample.java
+++ src/com/test/GitExample.java
@@ -2,9 +2,15 @@ package com.test;
 
 public class GitExample {
 
+	public void write() {
+		System.out.println("write");
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("hello git");
-
+		GitExample git=new GitExample();
+		git.write();
 	}
 
 }
