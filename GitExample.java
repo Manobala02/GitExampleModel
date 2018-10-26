9992402dbbcd3d6144cd86d4ec1071fc1a11285a
ssakthiselvakumar <44459246+ssakthiselvakumar@users.noreply.github.com>
2018-10-27 00:01:48 +0530

    Add files via upload

---
+++ GitExample.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class GitExample {
+
+	public void write() {
+		System.out.println("write");
+	}
+	public void Read() {
+		System.out.println("Read");
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("hello git");
+		GitExample git=new GitExample();
+		git.write();
+	}
+
+}
